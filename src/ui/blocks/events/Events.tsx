8bcc644272cb5a1be3ea03dda007ce3dcf04c5f1
Kew <wolff.kevin@protonmail.com>
2025-04-01 22:49:45 +0200

    Update event ui components

--- src/ui/blocks/events/Events.tsx
+++ src/ui/blocks/events/Events.tsx
@@ -18,55 +18,65 @@ export const Events: React.FC<Props> = ({ data }) => {
   }
 
   return (
-    <ul className="events">
-      {data.map((event, index) => (
-        <li
-          key={event.id}
-          className={clsx(
-            'events__item',
-            index === 0 ? 'events__item--highlight' : 'events__item--teaser',
-          )}
-        >
-          <div className="events__item__header">
-            <div className="events__item__header__top">
-              <h3>{event.title}</h3>
-              <p>{event['on-site'] ? 'présentiel' : 'visio conférence'}</p>
-            </div>
-            <div className="events__item__header__bottom">
-              <p>{formatdDate(event.date)}</p>
-              <p>{event.status.toLowerCase()}</p>
-            </div>
-          </div>
-          <div className="events__item__body">
-            <div className="events__item__body__content">
-              {index === 0 && (
-                <div className="events__item__body__content__description">{event.description}</div>
-              )}
-              <ul className="events__item__body__content__categories">
-                {event.categories?.map(
-                  (category) =>
-                    typeof category !== 'number' && (
-                      <li key={category.id}>
-                        <Category category={category} />
-                      </li>
-                    ),
-                )}
-              </ul>
-            </div>
-            {index == 0 && (
-              <div className="events__item__body__footer">
-                <Link href="#" variant="primary">
-                  S'inscrire
-                </Link>
-                <p>
-                  Il reste{' '}
-                  <span className="events__item__body__footer__cta">4 places disponibles</span>
-                </p>
-              </div>
+    <div className="events">
+      <h2 className="events__title">Événements à venir</h2>
+      <ul className="events__list">
+        {data.map((event, index) => (
+          <li
+            key={event.id}
+            className={clsx(
+              'events__list__item',
+              index === 0 ? 'events__list__item--highlight' : 'events__list__item--teaser',
             )}
-          </div>
-        </li>
-      ))}
-    </ul>
+          >
+            <div className="events__list__item__header">
+              <div className="events__list__item__header__top">
+                <h3>{event.title}</h3>
+                <p>{event['on-site'] ? 'présentiel' : 'visio conférence'}</p>
+              </div>
+              <div className="events__list__item__header__bottom">
+                <p>{formatdDate(event.date)}</p>
+                <p>{event.status.toLowerCase()}</p>
+              </div>
+            </div>
+            <div className="events__list__item__body">
+              <div className="events__list__item__body__content">
+                {index === 0 && (
+                  <div className="events__list__item__body__content__description">
+                    {event.description}
+                  </div>
+                )}
+                <ul className="events__list__item__body__content__categories">
+                  {event.categories?.map(
+                    (category) =>
+                      typeof category !== 'number' && (
+                        <li key={category.id}>
+                          <Category category={category} />
+                        </li>
+                      ),
+                  )}
+                </ul>
+              </div>
+              {index == 0 && (
+                <div className="events__list__item__body__footer">
+                  <Link href="#" variant="primary">
+                    S'inscrire
+                  </Link>
+                  <p>
+                    Il reste
+                    <span className="events__list__item__body__footer__cta">
+                      4 places disponibles
+                    </span>
+                  </p>
+                </div>
+              )}
+            </div>
+          </li>
+        ))}
+      </ul>
+      <Link href="#" variant="primary">
+        Voir tous les événements
+      </Link>
+    </div>
   )
 }
